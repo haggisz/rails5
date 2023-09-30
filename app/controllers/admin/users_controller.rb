385052d27d464fc5b8c018e5c42225a484828b82
haggisz <kurosuke96382@gmail.com>
2023-09-30 16:18:53 +0900

    ユーザ管理機能基礎 179p

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,18 @@
 class Admin::UsersController < ApplicationController
+  def index
+    @user = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
   end
 
   def create
@@ -13,6 +25,21 @@ class Admin::UsersController < ApplicationController
     end
   end
 
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to admin_user_url(@user), notice: "ユーザ「#{@user.name}」を更新しました。"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_user_url, notice: "ユーザ「#{@user.name}」を削除しました。"
+  end
   private
 
   def user_params
