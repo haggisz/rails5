e9ad4f400635c376f5a09ab72c465bbc477daf00
root <root@DESKTOP-S8MUJR1>
2023-10-02 16:24:36 +0900

    デスクトップで、プルリクエストのためにマージ

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
   namespace :admin do
     resources :users
   end
-  
+
   root 'tasks#index'
   resources :tasks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
